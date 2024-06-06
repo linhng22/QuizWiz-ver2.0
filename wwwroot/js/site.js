80ff98b17c102e239951479dfa0c1733c1f9f897
linhng2210 <k.linh2210@gmail.com>
2024-06-05 23:45:20 -0700

    Handle keydown events in flashcard div

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,6 +1,11 @@
 ﻿var currentCardIndex = 0;
 var flashcards = window.flashcards || [];
 
+var currentFlashcardElement = document.querySelector(
+  "#flashcard-count #current-flashcard-id"
+);
+var headerElement = document.querySelector("#flashcardHeader");
+
 function displayFlashcard(index) {
   var flashcard = flashcards[index];
   var questionElement = document.querySelector("#flashcard .question");
@@ -10,10 +15,10 @@ function displayFlashcard(index) {
   answerElement.textContent = flashcard.answer;
 
   // Initially show the question and hide the answer
-  var headerElement = document.querySelector("#flashcardHeader");
   headerElement.innerText = "Question";
   answerElement.style.display = "none";
   questionElement.style.display = "block";
+  currentFlashcardElement.innerText = currentCardIndex + 1;
 }
 
 function toggleFlashcard() {
@@ -35,22 +40,37 @@ function toggleFlashcard() {
   }
 }
 
-function nextFlashcard(callback) {
+function nextFlashcard() {
   console.log("Next flashcard called");
   if (currentCardIndex < flashcards.length - 1) {
     currentCardIndex++;
     displayFlashcard(currentCardIndex);
   }
-  callback();
 }
 
-function previousFlashcard(callback) {
+function previousFlashcard() {
   console.log("Previous flashcard called");
   if (currentCardIndex > 0) {
     currentCardIndex--;
     displayFlashcard(currentCardIndex);
   }
-  callback();
+}
+
+// Set the focus on the flashcard div element
+window.onload = function () {
+  document.getElementById("flashcard").focus();
+};
+
+// Handle keydown events on `flashcard` div
+function checkKey(event) {
+  if (event.key === "ArrowLeft") {
+    previousFlashcard();
+  } else if (event.key === "ArrowRight") {
+    nextFlashcard();
+  } else if (event.key === " ") {
+    event.preventDefault(); // avoid page scrolling
+    toggleFlashcard();
+  }
 }
 
 // Display the first flashcard initially
